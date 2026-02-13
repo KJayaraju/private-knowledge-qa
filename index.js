4b01e1e08653236278d83ff88ba4cbb94768e64f
KJayaraju <146174744+KJayaraju@users.noreply.github.com>
2026-02-13 18:05:29 +0530

    Change CORS origin to allow all origins

--- index.js
+++ index.js
@@ -7,11 +7,9 @@ dotenv.config();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "http://localhost:3000"
-  })
-);
+app.use(cors({
+  origin: "*"
+}));
 
 app.use(express.json());
 app.use("/", routes);
@@ -20,4 +18,5 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Backend running on port ${PORT}`);
+
 });
